90790445552663ef4485f939f4278af7895b77a0
EduardoLoran <eduardotormes@hotmail.com>
2022-06-08 18:41:45 -0300

    Alteração de input pt2

--- main.py
+++ main.py
@@ -9,8 +9,8 @@ releaseDateEnd = releaseDateEnd.split("/")
 releaseDateStart = releaseDateStart[2] + "-"  + releaseDateStart[1] + "-" + releaseDateStart[0]
 releaseDateEnd   = releaseDateEnd  [2] + "-"  + releaseDateEnd  [1] + "-" + releaseDateEnd  [0]
 
-#site = requests.get("https://www.imdb.com/search/title/?release_date=2000-01-01,2022-01-01")
-site = requests.get("https://www.imdb.com/search/title/?release_date=" + releaseDateStart + ","  + releaseDateEnd)
+site = requests.get("https://www.imdb.com/search/title/?release_date=2000-01-01,2022-01-01")
+#site = requests.get("https://www.imdb.com/search/title/?release_date=" + releaseDateStart + ","  + releaseDateEnd)
 
 page = bs4.BeautifulSoup(site.text ,"html.parser")
 
@@ -22,12 +22,12 @@ listYear = list()
 #Variable for amount of movie
 rangeMovie = int(input("Digite a quantidade de filmes que deseja buscar: \n"))
 
-
 for i in range(rangeMovie):
     listMove.append(page.select(".lister-item-header a")[i].getText())
     listVote.append(page.select('span[name="nv"]'      )[i].getText())
     listYear.append(page.select(".lister-item-year"    )[i].getText())
-
+    
+print(listVote)
 
 with open("Ranking.csv", "w") as arquivo:
     for movie, vote, year in zip(listMove, listVote , listYear):
