5c08322cecb5978cdc01ede58f5eb8fbbedb7527
EduardoLoran <eduardotormes@hotmail.com>
2022-06-08 12:50:06 -0300

    Alteração tabulacao

--- main.py
+++ main.py
@@ -1,31 +1,28 @@
 import requests, bs4
+import pandas as pd
 
 site = requests.get("https://www.imdb.com/search/title/?release_date=2000-01-01,2022-01-01")
 page = bs4.BeautifulSoup(site.text ,"html.parser")
 
-lista = list()
+filme = list()
+voto = list()
+ano = list()
+
 
 for i in range(3):
-    lista.append(page.select(".lister-item-header a")[i].getText())
-    lista.append(page.select('span[name="nv"]')[i].getText())
-    lista.append(page.select(".lister-item-year")[i].getText())
+    filme.append(page.select(".lister-item-header a")[i].getText())
+    voto.append(page.select('span[name="nv"]')[i].getText())
+    ano.append(page.select(".lister-item-year")[i].getText())
 
+lista= [filme,voto,ano]
+
+listaPrint = pd.DataFrame(lista, columns = ['Filme','Voto','Ano'])
 
 
 with open("Ranking.csv", "w") as arquivo:
-    arquivo.write("Filme;Voto;Ano\n")
-
-    for i in lista:
-        if lista[i] ==3:
-            arquivo.write("\n")
-
-print(lista)
-
-
-
+    arquivo.write(str(listaPrint))
 
 '''
-
 for char in "(–)":
     yearMovie = yearMovie.replace(char, "")
 '''
